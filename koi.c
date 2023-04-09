3a46c1fa9659a69b6aa190f93af5f93a3f892774
Aditya Pratama <aditya@hijup.com>
2023-04-09 20:53:02 +0700

    https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html#move-to-the-end-of-the-line-with-end

--- koi.c
+++ koi.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -363,7 +364,9 @@ void editorProcessKeycaps() {
       E.cx = 0;
       break;
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
+
       break;
     case PAGE_UP:
     case PAGE_DOWN:
